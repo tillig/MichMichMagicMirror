532abcbef470d9f860a92a9e4dadfede9b9407f0
Travis Illig <travis.illig@fiserv.com>
2019-08-26 16:00:51 -0700

    More calendars, news, etc.

--- config/config.js
+++ config/config.js
@@ -1,4 +1,6 @@
 ﻿const owApiKeyName = "OPENWEATHER_API_KEY";
+const tripitCalendarKeyName = "TRIPIT_CALENDAR_KEY";
+const tilligCalendarKeyName = "TILLIG_CALENDAR_KEY";
 
 var readEnvironment = function (name) {
   if (typeof process !== "undefined" && process.env[name]) {
@@ -26,22 +28,28 @@ var config = {
     },
     {
       module: "clock",
-      position: "top_left"
-    },
-    {
-      module: "calendar",
-      header: "US Holidays",
       position: "top_left",
       config: {
-        calendars: [{
-          symbol: "calendar-check",
-          url: "webcal://www.calendarlabs.com/ical-calendar/ics/76/US_Holidays.ics"
-        }]
+        timezone: "America/Los_Angeles"
       }
     },
     {
-      module: "compliments",
-      position: "lower_third"
+      module: "calendar",
+      header: "Upcoming Events",
+      position: "top_left",
+      config: {
+        maximumNumberOfDays: 7,
+        calendars: [{
+          symbol: "campground",
+          url: "webcal://www.calendarlabs.com/ical-calendar/ics/76/US_Holidays.ics"
+        }, {
+          symbol: "plane",
+          url: "https://www.tripit.com/feed/ical/private/" + readEnvironment(tripitCalendarKeyName) + "/tripit.ics"
+        }, {
+          symbol: "male",
+          url: "https://calendar.google.com/calendar/ical/" + readEnvironment(tilligCalendarKeyName) + "/basic.ics"
+        }]
+      }
     },
     {
       // ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
@@ -70,8 +78,12 @@ var config = {
       position: "bottom_bar",
       config: {
         feeds: [{
-          title: "New York Times",
-          url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+          title: "Washington Post - Politics",
+          url: "http://feeds.washingtonpost.com/rss/politics"
+        },
+        {
+          title: "Washington Post - National",
+          url: "http://feeds.washingtonpost.com/rss/national"
         }],
         showSourceTitle: true,
         showPublishDate: true,
